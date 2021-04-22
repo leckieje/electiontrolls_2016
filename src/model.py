ab8cc9c398e11ff9d9b87c5aab28d628a610c07f
leckieje <leckieje@gmail.com>
2021-04-22 19:48:20 -0400

    few morer

--- src/model.py
+++ src/model.py
@@ -48,7 +48,7 @@ def get_data(num_samples=50000, split=True, balanced=False):
 
         if balanced:
             troll_samp = get_random_sample(troll_summer, (num_samples/2))
-            legit_samp = get_random_sample(legit, (num_samples/2)
+            legit_samp = get_random_sample(legit, (num_samples/2))
         else:
             troll_samp = get_random_sample(troll_summer, int(0.08 * num_samples))
             legit_samp = get_random_sample(legit, int((1 - 0.08) * num_samples))
