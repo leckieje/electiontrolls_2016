a4205f7ecfa3a57f8a66386d4e61467772243c3d
leckieje <leckieje@gmail.com>
2021-04-22 17:51:32 -0400

    function to balance classes

--- src/model.py
+++ src/model.py
@@ -10,6 +10,19 @@ custom_stops = ['https', 'rt', 'co', 'amp', 'via', 'go', 'get', 'said', 'say', '
                 'trump', 'clinton', 'donald', 'donald trump', 'donaldtrump', 'says', 'hillary', 'hillaryclinton',
                 'hillary clinton', 'realdonaldtrump', 'would', 'let', 'video', 'like']
 
+# clean tweets hydrated from GWU
+def clean_hydrated(filepath):  
+    df = pd.read_csv(filepath)
+    df = df[df['lang'] == 'en']
+    df = df.loc[:, ['created_at', 'user_screen_name', 'text', 'user_location', 'retweet_count', 'favorite_count']]
+    df.rename(columns={'created_at': 'date', 'user_screen_name': 'user', 'text': 'text', 'user_location': 'location', 
+                       'retweet_count': 'retweets', 'favorite_count': 'favs'}, inplace=True)
+    df.drop_duplicates(ignore_index=True, inplace=True)
+    df['date'] = pd.to_datetime(df['date'], infer_datetime_format=True)
+    df['legit'] = 1
+    
+    return df
+
 # get random samples
 def get_random_sample(df, num_samples):
     samp_idx = np.random.choice(range(len(df)), size=num_samples, replace=False)
@@ -17,7 +30,7 @@ def get_random_sample(df, num_samples):
     return df_samp
 
 # load and split data
-def get_data(num_samples=50000, split=True):
+def get_data(num_samples=50000, split=True, balanced=False):
     # load data
     legit = pd.read_csv('data/legit_tweets.csv', parse_dates = ['date'])
     troll = pd.read_csv('data/troll_tweets.csv', parse_dates = ['date'])
@@ -32,8 +45,13 @@ def get_data(num_samples=50000, split=True):
     #     troll_samp = get_random_sample(troll_summer, int(num_samples/2))
     #     legit_samp = get_random_sample(legit, int(num_samples/2))
     if num_samples > 0:
-        troll_samp = get_random_sample(troll_summer, int(0.08 * num_samples))
-        legit_samp = get_random_sample(legit, int((1 - 0.08) * num_samples))
+
+        if balanced:
+            troll_samp = get_random_sample(troll_summer, num_samples)
+            legit_samp = get_random_sample(legit, num_samples)
+        else:
+            troll_samp = get_random_sample(troll_summer, int(0.08 * num_samples))
+            legit_samp = get_random_sample(legit, int((1 - 0.08) * num_samples))
     else:
         troll_samp = troll_summer
         legit_samp = legit
