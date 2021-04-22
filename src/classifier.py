7d01c750b6625047f68e3d0eb29cdcf8b90a4e77
leckieje <leckieje@gmail.com>
2021-04-22 17:38:38 -0400

    f1 score

--- src/classifier.py
+++ src/classifier.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, roc_curve, recall_score, precision_score, accuracy_score
+from sklearn.metrics import confusion_matrix, roc_curve, recall_score, precision_score, accuracy_score, f1_score
 from sklearn.inspection import permutation_importance, plot_partial_dependence, partial_dependence
 
 
